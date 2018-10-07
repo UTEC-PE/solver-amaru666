69ca07ad109402e32e3d361525d0e6fa3951218d
Luis Talavera <luis@yodopay.com>
2018-10-07 16:54:41 -0500

    Comentarios

--- expressiontree.cpp
+++ expressiontree.cpp
@@ -5,8 +5,21 @@
 #include <iostream>
 #include "expressiontree.h"
 using namespace std;
-int main() {
-    string expresion = "(-4-(-4))+8+16";
+
+// Solo resuelve sumas y restas
+// Falta multiplicación, división, variables, unarios
+// No combina bien los operadores, e.g. 5+--4
+
+int main(int argc, char const *argv[]) {
+    string expresion;
+    if (argc == 2) {
+        expresion = argv[1];
+    }
+    else {
+      throw "One argument expected";
+    }
+    //string expresion = "(-4-(-4))+8+16";
     node *bicho = build(expresion);
-    cout<<bicho->result();
+    cout<<bicho->result()<<endl;
+    return 0;
 }
